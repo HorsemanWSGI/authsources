0946099cc82329e2ed5aa8ab95a167f3fc6302db
Souheil Chelfouh <trollfot@gmail.com>
2026-03-16 11:01:08 +0100

    JSON validation now raises ValidationErrors

--- src/authsources/json.py
+++ src/authsources/json.py
@@ -1,6 +1,21 @@
 import jsonschema_rs
 from frozendict import frozendict
 from functools import cached_property
+from jsonschema_rs import ValidationError
+
+
+class ValidationErrors(Exception):
+
+    errors: list[ValidationError]
+
+    def __init__(self, *errors: ValidationError):
+        self.errors: List[ValidationError] = list(errors)
+
+    def __iter__(self):
+        return iter(self.errors)
+
+    def __len__(self):
+        return len(self.errors)
 
 
 class JSONSchema(frozendict):
@@ -9,5 +24,7 @@ class JSONSchema(frozendict):
     def validator(self):
         return jsonschema_rs.validator_for(self)
 
-    def validate(self, instance):
-        yield from self.validator.iter_errors(instance)
+    def validate(self, instance) -> ValidationErrors | None:
+        errors = list(self.validator.iter_errors(instance))
+        if errors:
+            raise ValidationErrors(*errors)
