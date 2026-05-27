fb5230fa2f293f5273f8194c7d2e425c2b673120
Souheil Chelfouh <trollfot@gmail.com>
2026-05-27 12:36:45 +0200

    Fixed actions

--- tests/test_dict_source.py
+++ tests/test_dict_source.py
@@ -1,4 +1,5 @@
-from authsources.sources.mapping import DictSource, DictUser
+from authsources.sources.mapping import DictSource, DictUser, Login
+from authsources import protocols
 
 
 def test_dict_source_base():
@@ -35,3 +36,21 @@ def test_dict_source_bindings():
     assert source.bindings == {}
     assert bound.bindings == {"request": "Whatever"}
     assert bound.users is source.users
+
+
+def test_dict_source_actions():
+    source = DictSource(
+        users={
+            "test": {
+                "password": "test"
+            },
+        },
+        title="test",
+        description="Test of DictSource",
+        actions=(Login,)
+    )
+    action = source.get(protocols.Challenge)
+    assert isinstance(action, Login)
+
+    action = source.get(protocols.Getter)
+    assert action is None
